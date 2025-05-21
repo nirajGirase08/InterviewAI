d064a4eb93f933258ac5c790f0255d3ea06d6eb6
Niraj Girase <nirajgirase25@gmail.com>
2025-05-21 15:09:50 -0500

    Tried the spacy en_core_web_lg large model instead of medium to get more accurate results and speed up the task

--- Resume_Parsing/Resume_parsing.py
+++ Resume_Parsing/Resume_parsing.py
@@ -4,10 +4,8 @@ import sys, fitz
 import pandas as pd
 import numpy as np
 
-"""
-You can comment the code from line 10 to 23 once u have trained the model 
-"""
-df_resume = pd.read_csv("C:/Users/kenchatg/Softwares/Sem2/ProjInAI/InterviewAI/Resume_Parsing/Resume/Resume.csv") #Path of the Resume folder
+
+df_resume = pd.read_csv("/Users/neel/Documents/Projects/InterviewAI/Resume_Parsing/Resume.csv") #Path of the Resume folder
 
 #randomized Job categories so that 200 samples contain various job categories instead of one.
 df_resume = df_resume.reindex(np.random.permutation(df_resume.index))
@@ -16,10 +14,12 @@ df_resume = df_resume.reindex(np.random.permutation(df_resume.index))
 df_resume = df_resume.copy().iloc[0:200,]
 
 
-nlp = spacy.load("en_core_web_md")
-skill_pattern_path = "C:/Users/kenchatg/Softwares/Sem2/ProjInAI/InterviewAI/Resume_Parsing/jz_skill_patterns.jsonl" #path to jz_skill_patterns.jsonl file
+# nlp = spacy.load("en_core_web_md")
+nlp = spacy.load("en_core_web_lg")
 
-ruler = nlp.add_pipe("entity_ruler")
+skill_pattern_path = "/Users/neel/Documents/Projects/InterviewAI/Resume_Parsing/skill_patterns.jsonl" #path to skill_patterns.jsonl file
+
+ruler = nlp.add_pipe("entity_ruler") #Not added by default so meed to add entity ruler seperately
 ruler.from_disk(skill_pattern_path)
 
 def remove_hyperlinks(sentence):
@@ -46,11 +46,11 @@ def preprocessing(sentence):
             token.pos_ != 'PUNCT' and token.pos_ != 'SPACE':
             cleaned_tokens.append(token.lemma_.lower().strip())
             
-    #instead of returning tokens, we shall join them
+    #instead of returning tokens, join them and return
     return " ".join(cleaned_tokens)
 
 # Below is the path of the dummy resume to be tested
-fname = '/Users/neel/Documents/Projects/InterviewAI/Resume/Alice_Clark_CV.pdf'
+fname = '/Users/neel/Documents/Projects/InterviewAI/Resume_Parsing/TestResume.pdf'
 doc = fitz.open(fname)
 text = ""
 for page in doc:
@@ -64,3 +64,4 @@ skills = [ent.text for ent in doc.ents if ent.label_ == "SKILL"]
 
 # Print unique skills
 print(set(skills))
+# print(nlp.pipe_names)
